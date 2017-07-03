b528eda29e16f8fcda182f423eeb6e80a785f2c0
846762278@qq.com <846762278@qq.com>
2017-07-03 16:28:49 +0800

    新增页面组件

---
+++ demo1/src/components/DubleData.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-row>
+        <el-col>
+            <h2>数据双向绑定</h2>
+        </el-col>
+    </el-row>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
