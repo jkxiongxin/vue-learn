4fe6c8ee0cd334601c1e23c66212940adb2503e9
846762278@qq.com <846762278@qq.com>
2017-07-03 17:19:57 +0800

    修改文件名称 添加双向数据绑定示例

--- demo1/src/components/DubleData.vue
+++ demo1/src/components/DubleData.vue
@@ -2,6 +2,12 @@
     <el-row>
         <el-col>
             <h2>数据双向绑定</h2>
+            <el-form>
+                <el-form-item label="输入">
+                    <el-input v-model="name"></el-input>
+                </el-form-item>
+            </el-form>
+            当前输入为：{{name}}
         </el-col>
     </el-row>
 </template>
@@ -9,6 +15,7 @@
 export default {
   data () {
     return {
+        name: ''
     }
   }
 }
