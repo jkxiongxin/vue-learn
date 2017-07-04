631f209f89be92e748590aec0971645e49153117
ytshang123 <2370718210@qq.com>
2017-07-04 22:34:19 +0800

    Test

---
+++ demo2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<el-row class="tac">
+    <el-col>
+      <el-menu default-active="/" class="el-menu-demo" :router="true" mode="horizontal" theme="dark">
+        <el-menu-item index="/">首页</el-menu-item>
+        <el-menu-item index="/data">双向绑定</el-menu-item>
+      </el-menu>
+    </el-col>
+    <el-col>
+      <router-view></router-view>
+    </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body{
+  margin:0
+}
+</style>
---
+++ demo2/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Data from '@/components/Data'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },{
+    	path:'/data',
+    	name:'Data',
+    	component:Data
+    }
+  ]
+})
